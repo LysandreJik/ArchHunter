8c056704a2117f2abd4f5811d99d2fce31214045
Lysandre DEBUT <lysandre.debut@reseau.eseo.fr>
2018-07-10 04:58:34 +0200

    Allow for zone selection, and updates db on click.

--- components/splash/ZoneSelection.js
+++ components/splash/ZoneSelection.js
@@ -1,14 +1,60 @@
 import React from 'react'
-import {View, Text, Button} from 'react-native';
+import {ScrollView, View, Text, Button} from 'react-native';
+import {NotificationsManagement} from "../../usermanagement/NotificationsManagement";
+
+var AreasInformation = require('../../assets/jsonFiles/AreasInformation');
 
 export default class ZoneSelection extends React.Component{
+
+    startHunt(zone){
+        console.log("Starting hunt in zone : ", zone);
+        let not = new NotificationsManagement({positions: AreasInformation[zone].positions, username: this.props.username, password: this.props.password, zone: zone});
+    }
+
     render(){
+        let parent = this;
         return(
-            <View>
-                <Text>
+            <View style={styles.container}>
+                <Text style={styles.text}>
                     Select a zone
                 </Text>
+                <ScrollView>
+                    {Object.keys(AreasInformation).map(function(key, index){
+                        return <Button key={index} title={key} onPress={() => parent.startHunt(key)}/>
+                    })}
+                </ScrollView>
             </View>
         )
+    }
+}
+
+const styles = {
+    container: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1,
+        backgroundColor: "#0B0C10",
+        paddingTop: 50,
+        paddingBottom: 50
+    },
+
+    textInput: {
+        width: "90%",
+        borderWidth: 1,
+        borderColor: "#45a29e",
+        backgroundColor: "#1F2833",
+        height: 40,
+        margin: 5,
+        padding: 5
+    },
+
+    text: {
+        color: "#66FCF1",
+        fontSize: 20,
+        margin: 5
+    },
+
+    scrollView: {
+
     }
 }
