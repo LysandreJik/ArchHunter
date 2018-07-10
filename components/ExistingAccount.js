dd7ab3bbf69b3d5d8dc318f43c2b122fca67839c
Lysandre DEBUT <lysandre.debut@reseau.eseo.fr>
2018-07-10 05:24:01 +0200

    Existing Account selection is working

---
+++ components/ExistingAccount.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import {View, Text, Button, ActivityIndicator} from 'react-native';
+
+const PUSH_ENDPOINT = 'https://datbot.lysandredebut.fr/app/php/FetchExistingAccounts.php';
+
+export default class ExistingAccount extends React.Component{
+
+    state = {dataFetched: false};
+
+    constructor(){
+        super();
+
+        fetch(PUSH_ENDPOINT, {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            }
+        })
+            .then(response => response.json())
+            .then(data => this.data = data)
+            .then(() => this.setState({dataFetched: true}))
+            .done();
+    }
+
+    clickedOnName(username, password){
+        console.log("Clicked on ", username, password);
+
+        this.props.selectZone(username, password);
+    }
+
+    render(){
+        let parent = this;
+        if(this.state.dataFetched){
+            if(this.data.length === 0){
+                return(
+                    <View><Text style={styles.text}>No registered accounts.</Text></View>
+                )
+            }else{
+                return(
+                    <View>{this.data.map(function(item, key){return <Button key={key} title={item.username} onPress={() => parent.clickedOnName(item.username, item.password)}/>})}</View>
+                )
+            }
+        }else{
+            return(
+                <ActivityIndicator size="large" color="#66FCF1"/>
+            );
+        }
+
+    }
+}
+
+const styles = {
+    container: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1,
+        backgroundColor: "#0B0C10"
+    },
+
+    textInput: {
+        width: "90%",
+        borderWidth: 1,
+        borderColor: "#45a29e",
+        backgroundColor: "#1F2833",
+        height: 40,
+        margin: 5,
+        padding: 5
+    },
+
+    text: {
+        color: "#66FCF1",
+        fontSize: 20,
+        margin: 5
+    }
+}
